1cfb030d894f25b4de7d2e0a06b1c628c8b74e1e
Brn <bruno.trabalhos@outlook.com>
2023-06-20 19:11:00 -0300

    Criada seleção de player 1 e player 2

--- App.java
+++ App.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Deus é minha fortaleza");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Welcome to the tic-tac-toe game :)");
+        System.out.println();
+
+        System.out.print("Player 1 X or O ? -> ");
+        char p1, p2;
+        p1 = sc.next().charAt(0);
+        if (p1 != 'X' && p1 != 'O'){
+            do{
+                System.out.print("Select valid player: X or O...");
+                p1 = sc.next().charAt(0);
+            }while(p1 != 'X' && p1 != 'O');
+        }
+        if(p1 == 'X'){
+            p2 = 'O';
+        }else{
+            p2 = 'X';
+        }
+        System.out.println("player 1: " + p1 + " - player 2: " + p2);                
+        sc.close();
     }
 }
