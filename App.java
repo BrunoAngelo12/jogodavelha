22403957224830045420c7455f4f0518435a5ac0
Brn <bruno.trabalhos@outlook.com>
2023-06-26 20:32:24 -0300

    Criado a possibilidade de jogar nas nove posicoes

--- App.java
+++ App.java
@@ -33,8 +33,8 @@ public class App {
         tab.newBoard();
         tab.showBoard();
         System.out.print("Insert: ");
-        int select = sc.nextInt();
-
+        char insert = sc.next().charAt(0);
+        tab.insertMove(insert, p1);
 
         sc.close();
     }
--- Tabuleiro.java
+++ Tabuleiro.java
@@ -1,5 +1,5 @@
 public class Tabuleiro {
-    public Integer[][] board = new Integer[3][3];
+    public Character[][] board = new Character[3][3];
 
     public Tabuleiro(){}
 
@@ -18,15 +18,52 @@ public class Tabuleiro {
     }
     
     public void newBoard(){
-        this.board[0][0] = 1;
-        this.board[0][1] = 2;
-        this.board[0][2] = 3;
-        this.board[1][0] = 4;
-        this.board[1][1] = 5;
-        this.board[1][2] = 6;
-        this.board[2][0] = 7;
-        this.board[2][1] = 8;
-        this.board[2][2] = 9;       
+        this.board[0][0] = '1';
+        this.board[0][1] = '2';
+        this.board[0][2] = '3';
+        this.board[1][0] = '4';
+        this.board[1][1] = '5';
+        this.board[1][2] = '6';
+        this.board[2][0] = '7';
+        this.board[2][1] = '8';
+        this.board[2][2] = '9';       
+    }
+
+    public void insertMove(char insert, char player){
+        switch (insert){
+            case '1':
+                this.board[0][0] = player;
+                break;
+            case '2':
+                this.board[0][1] = player;
+                break;
+            case '3':
+                this.board[0][2] = player;
+                break;
+            case '4':
+                this.board[1][0] = player;
+                break;
+            case '5':
+                this.board[1][1] = player;
+                break;
+            case '6':
+                this.board[1][2] = player;
+                break;
+            case '7':
+                this.board[2][0] = player;
+                break;
+            case '8':
+                this.board[2][1] = player;
+                break;
+            case '9':
+                this.board[2][2] = player;
+                break;
+            default:
+                System.out.println("Jogada inválida");
+                break;                             
+        }
+        System.out.println();
+        showBoard();   
     }
 
 }
